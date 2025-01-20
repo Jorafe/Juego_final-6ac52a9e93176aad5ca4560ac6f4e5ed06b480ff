0e56b8b0e3595a818f2f417248f281a2f722e23f
Jorafe <joanrafe15@gmail.com>
2025-01-20 11:23:57 +0100

    2 sided

--- Assets/Scripts/Orden Gerarquia/HierarchyHeaderEditor.cs
+++ Assets/Scripts/Orden Gerarquia/HierarchyHeaderEditor.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HierarchyHeaderEditor : MonoBehaviour
+/*public class HierarchyHeaderEditor : MonoBehaviour
 {
 
     public static readonly Color DEFAULT_BACKGROUND_COLOR = Color.grey;
 
     public Color backgroundColor = DEFAULT_BACKGROUND_COLOR;
-}
+}*/
