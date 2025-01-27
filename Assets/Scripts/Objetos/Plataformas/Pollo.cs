8e7cba0838ef5b36eb4e9b22a8c809bcf436c297
Joan <joanrafe15@gmail.com>
2025-01-27 18:28:05 +0100

    Mejora primer nivel plataformas

---
+++ Assets/Scripts/Objetos/Plataformas/Pollo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pollo : MonoBehaviour
+{
+    public float fuerzaRebote = 10f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            // Aplica una fuerza hacia arriba
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); // Reiniciar velocidad vertical
+            rb.AddForce(Vector3.up * fuerzaRebote, ForceMode.Impulse);
+        }
+    }
+}
